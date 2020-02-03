68ed3fe97fc9ec896e6534e6aaa8d6f5085fb298
Philipe Goulet <phil.gou69@hotmail.com>
2020-02-03 16:48:09 -0500

    Built test program

---
+++ TestVecteur.cpp
@@ -0,0 +1,24 @@
+#include "vecteur.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "TEST" << endl;
+    Vecteur<int> vecteurTest;
+    //cout << vecteurTest.getSize() << endl;
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    vecteurTest.add(i);
+    //}
+
+    //cout << vecteurTest.getSize() << endl;
+    //cout << "TEST" << endl;
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    cout << vecteurTest.get(i) << endl;
+    //}
+    return 0;
+}
